d45c6cb98db4738a640b2e64882d1bd1fa8f2a1d
Luís Soares <gersonluis1209@gmail.com>
2024-09-08 01:17:34 -0300

    Criada classes de serviço e o componente de registro

--- src/main/java/com/devtocode/ecommerce/resources/UserResource.java
+++ src/main/java/com/devtocode/ecommerce/resources/UserResource.java
@@ -1,32 +1,74 @@
 package com.devtocode.ecommerce.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devtocode.ecommerce.entities.User;
+import com.devtocode.ecommerce.services.UserServices;
 
 @RestController
 @RequestMapping(value ="/users")
 public class UserResource {
+	
+	
+	@Autowired
+	private UserServices service;
+	
 
 		@GetMapping
-		public ResponseEntity<User> searchUser(){
+		public ResponseEntity<List<User>> searchUser(){
 			
-			User user = new User(1L, "Teste", "teste@gmail.com", "32154078", "32154078");
+			
+			List<User> list = service.findAll();
 		
 			
-			return ResponseEntity.ok().body(user);
+			return ResponseEntity.ok().body(list);
 			
-			
-		
 
+		
+		}
+		
+		
+		
+		@GetMapping(value = "/{id}")
+		public ResponseEntity<User> findById(@PathVariable Long id){
+			
+			User obj = service.findById(id);
+			
+			return ResponseEntity.ok().body(obj);
+			
 			
 			
 		}
+		
 	
 	
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
